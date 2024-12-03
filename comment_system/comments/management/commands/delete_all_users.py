9132b594dac99f79a4940370b2e64c4db6dae945
Hania Salman <162374653+haniSalm@users.noreply.github.com>
2024-12-03 23:59:58 +0500

    Delete comment_system directory

--- comment_system/comments/management/commands/delete_all_users.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from authapp.models import CustomUser  # Adjust according to the actual location of CustomUser
-
-class Command(BaseCommand):
-    help = 'Delete all users from the database'
-
-    def handle(self, *args, **kwargs):
-        CustomUser.objects.all().delete()
-        self.stdout.write(self.style.SUCCESS('Successfully deleted all users'))
